d13c416e35e3552c0b4a5f822978eb662feb0d37
Tom Rankenberg <tomrankenberg@live.nl>
2025-01-23 21:41:09 +0100

    Added database, need to add tables

--- Data/Context/DbContext.cs
+++ Data/Context/DbContext.cs
@@ -7,6 +7,8 @@ public class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions<DatabaseContext> options)
     : base(options)
     {
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.Entity<StravaUser>().HasKey(x => x.UserId);
     }
 
     public DbSet<StravaUser> Users { get; set; }
