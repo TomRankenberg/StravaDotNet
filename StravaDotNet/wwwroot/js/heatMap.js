8dda1ba911e5edccdc7ab14e342f713a50bf4fdc
Tom Rankenberg <tomrankenberg@live.nl>
2025-02-03 21:52:09 +0100

    Map loads, polylines dont appear yet

---
+++ StravaDotNet/wwwroot/js/heatMap.js
@@ -0,0 +1,22 @@
+﻿function load_map() {
+    var map = L.map('map').setView([52.09064, 5.12130], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
+function initializeHeatMap(polylines) {
+    var map = L.map('map').setView([52.09064, 5.12130], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    polylines.forEach(function (polyline) {
+        var latlngs = L.PolylineUtil.decode(polyline);
+        L.polyline(latlngs, { color: 'red' }).addTo(map);
+    });
+}
+
+
