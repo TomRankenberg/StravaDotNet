44d662c22d00c926ee4f0396d4ad8da6cc97a702
Tom Rankenberg <tomrankenberg@live.nl>
2025-04-21 14:49:54 +0200

    Added vm for activity

---
+++ StravaDotNet/ViewModels/ActivityVm.cs
@@ -0,0 +1,10 @@
+﻿using Data.Models.Strava;
+
+namespace StravaDotNet.ViewModels
+{
+    public class ActivityVm
+    {
+        public DetailedActivity Activity { get; set; }
+        public double AverageHeartRate { get; set; }
+    }
+}
