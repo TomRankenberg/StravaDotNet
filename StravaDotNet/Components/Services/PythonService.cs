27c146fdb172b9b0d56510d75d42826dacb75ba7
Tom Rankenberg <tomrankenberg@live.nl>
2025-05-20 22:25:27 +0200

    Added python project

---
+++ StravaDotNet/Components/Services/PythonService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using StravaDotNet.ViewModels;
+using System.Text;
+
+namespace StravaDotNet.Components.Services
+{
+    public class PythonService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PythonService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> PredictAsync(List<ActivityVm> activities)
+        {
+            var json = JsonConvert.SerializeObject(activities);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("http://localhost:5000/predict", content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+}
