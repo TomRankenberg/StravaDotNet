4a4db869c446740efa26751067738ce55a31da95
Tom Rankenberg <tomrankenberg@live.nl>
2025-05-25 18:00:14 +0200

    Added comment

--- StravaDotNet/Components/Services/PythonService.cs
+++ StravaDotNet/Components/Services/PythonService.cs
@@ -14,7 +14,7 @@ namespace StravaDotNet.Components.Services
         }
 
         public async Task<string> PredictAsync(List<ActivityVm> activities)
-        {
+        {// Doesnt work yet, not implemented
             var json = JsonConvert.SerializeObject(activities);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("http://localhost:5000/predict", content);
