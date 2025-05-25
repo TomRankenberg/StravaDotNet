23381717b6d5019404aebeda9b1576d87a9742da
Tom Rankenberg <tomrankenberg@live.nl>
2025-05-25 18:03:54 +0200

    Removed old files

--- regression/DataParser.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    # process data...
-    return jsonify({'result': 'ok'})
