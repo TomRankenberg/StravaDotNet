abbfddbb15f5f5ef12d8abc505b7335100c12ef8
Tom Rankenberg <tomrankenberg@live.nl>
2026-01-03 13:32:06 +0100

    First auth0 implementation

---
+++ StravaDotNet/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StravaDotNet.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            var props = new AuthenticationProperties { RedirectUri = returnUrl };
+            // "Auth0" is the scheme name used by Auth0.AspNetCore.Authentication
+            return Challenge(props, "Auth0");
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            var callbackUrl = Url.Content("~/");
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties { RedirectUri = callbackUrl });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect(callbackUrl);
+        }
+    }
+}
